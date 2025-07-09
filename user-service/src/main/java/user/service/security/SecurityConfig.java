3375ec1a7bb289431c906c89222769dd1a2504ca
ponir-saha <ponir.saha@gmail.com>
2025-07-09 10:16:12 +0600

    fixed docker issue and added jwt

--- user-service/src/main/java/user/service/security/SecurityConfig.java
+++ user-service/src/main/java/user/service/security/SecurityConfig.java
@@ -4,7 +4,6 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -18,7 +17,7 @@ import java.io.IOException;
 
 @Component
 @RequiredArgsConstructor
-public class JwtAuthenticationFilter extends OncePerRequestFilter {
+public class SecurityConfig extends OncePerRequestFilter {
 
     private final JwtService jwtService;
     private final UserDetailsService userDetailsService;
